1ae26c4acae9845230abd9584e85ca79b65de16a
renjinhui <1227760869@qq.com>
2019-08-27 13:06:29 +0800

    day2上午

---
+++ week2/day2/练习.js
@@ -0,0 +1,28 @@
+function Foo() {
+    getName = function () {
+        console.log(1);
+    };
+    return this;
+}
+Foo.getName = function () {
+    console.log(2);
+}; 
+Foo.prototype.getName = function () {
+    console.log(3);
+};
+var getName = function () {
+    console.log(4);
+};
+
+function getName() {
+    console.log(5);
+}
+
+Foo.getName();
+getName();
+Foo().getName();
+getName();
+var a = new Foo.getName();
+var b = new Foo().getName();
+var c = new new Foo().getName();
+console.log(a, b, c);
