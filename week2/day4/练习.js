2b82e2b754495c0b0ebb5e99bbfef24e5c8a7b74
renjinhui <1227760869@qq.com>
2019-08-31 20:14:25 +0800

    day5

--- week2/day4/练习.js
+++ week2/day4/练习.js
@@ -1,27 +1,27 @@
 var obj = {
     name: " jsCoder",
-    skill: ["css3","html5", "es6", "react", "angular"],
+    skill: ["css3", "html5", "es6", "react", "angular"],
     say: function () {
-        for(let i = 0, len = this.skill.length; i< len; i++){
-            setTimeout(()=>{
+        for (let i = 0, len = this.skill.length; i < len; i++) {
+            setTimeout(() => {
                 console.log("No." + i + this.name);
                 console.log(this.skill[i]);
                 console.log('--------------------------');
-            },100);
+            }, 100);
         }
     }
 }
-obj.say();// say中的this是obj
+obj.say(); // say中的this是obj
 
 
-var F = function(){};
-Object.prototype.a=function(){
+var F = function () {};
+Object.prototype.a = function () {
     console.log('a()')
 };
-Function.prototype.b=function(){
+Function.prototype.b = function () {
     console.log('b()')
 };
-var f=new F();
+var f = new F();
 f.a();
 f.b();
 F.a();
@@ -42,10 +42,10 @@ Foo.prototype.getName = function () {
 var getName = function () {
     console.log(4);
 };
+
 function getName() {
     console.log(5);
 }
-
 Foo.getName();
 getName();
 Foo().getName();
@@ -53,13 +53,15 @@ getName();
 var a = new Foo.getName();
 var b = new Foo().getName();
 var c = new new Foo().getName();
-console.log(a,b,c);
+console.log(a, b, c);
 
 
-var name = '中国',age = 5000;
+var name = '中国',
+    age = 5000;
+
 function Person() {
     getName = function () {
-      console.log(this.name)
+        console.log(this.name)
     };
     this.name = 'zfpx';
     this.age = 9;
@@ -72,13 +74,90 @@ Person.getName = function () {
 Person.prototype.getName = function () {
     console.log(this.name);
 };
-var obj = {name:'培训',f:Person};
+var obj = {
+    name: '培训',
+    f: Person
+};
 var per = new Person();
-per.getName = function(){
+per.getName = function () {
     console.log(12)
 };
 var per2 = new Person();
-f();
+obj.f();
 Person();
 per.getName();
 per2.getName();
+
+
+function
+foo()
+
+{
+
+
+    this
+        .
+    some
+        =
+
+        '222'
+
+    let ccc =
+
+        'ccc'
+
+    foo
+        .
+    obkoro1
+        =
+
+        'obkoro1'
+
+    foo
+        .
+    prototype
+        .
+    a
+        =
+
+        'aaa'
+
+}
+
+foo
+    .
+koro
+    =
+
+    '扣肉'
+
+foo
+    .
+prototype
+    .
+test
+    =
+
+    'test'
+
+let foo1 =
+
+    new
+foo
+    ()
+
+// `foo1`上有哪些属性,这些属性分别挂载在哪个地方
+
+foo
+    .
+prototype
+    .
+test
+    =
+
+    'test2'
+
+// 重新赋值
+console.log(foo1.test)
+console.log(foo.koro)
+console.log(foo1.koro)
